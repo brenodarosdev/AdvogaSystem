253295fa79795b83cfb48fa5f9968e6348ceff73
Breno Daros <brenosouzadaros@gmail.com>
2024-03-21 18:48:49 -0300

    trata exception derivada da tentativa de salvamento de um email ja existente no banco

--- src/main/java/com/AdvogaSystem/advogado/infra/AdvogadoInfraRepository.java
+++ src/main/java/com/AdvogaSystem/advogado/infra/AdvogadoInfraRepository.java
@@ -4,9 +4,12 @@ import com.AdvogaSystem.advogado.application.api.AdvogadoCriadoResponse;
 import com.AdvogaSystem.advogado.application.api.AdvogadoRequest;
 import com.AdvogaSystem.advogado.application.repository.AdvogadoRepository;
 import com.AdvogaSystem.advogado.domain.Advogado;
+import com.AdvogaSystem.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.catalina.Store;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 @Log4j2
@@ -18,8 +21,12 @@ public class AdvogadoInfraRepository implements AdvogadoRepository {
     @Override
     public Advogado salva(Advogado advogado) {
         log.info("[inicia] AdvogadoInfraRepository - salva");
-        Advogado advogadoSalvo = advogadoSpringDataMongoDBRepository.save(advogado);
-        log.info("[finaliza] AdvogadoInfraRepository - salva");
-        return advogadoSalvo;
+        try {
+            Advogado advogadoSalvo = advogadoSpringDataMongoDBRepository.save(advogado);
+            log.info("[finaliza] AdvogadoInfraRepository - salva");
+            return advogadoSalvo;
+        } catch (DataIntegrityViolationException e) {
+            throw APIException.build(HttpStatus.BAD_REQUEST, "Já existe um advogado cadastrado com este email!");
+        }
     }
 }
