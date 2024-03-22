4e857580d98444180d721a10bf443a28ab55a8b5
Breno Daros <brenosouzadaros@gmail.com>
2024-03-22 13:02:03 -0300

    implementa dominio de advocacia

---
+++ src/main/java/com/AdvogaSystem/advocacia/domain/Advocacia.java
@@ -0,0 +1,32 @@
+package com.AdvogaSystem.advocacia.domain;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Getter
+@Document(collection = "advocacia")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Advocacia {
+    @Id
+    private UUID idAdvocacia;
+    @Email(message = "Este campo deve conter um email")
+    @Indexed(unique = true)
+    @NotBlank(message = "O campo email não pode estar em branco")
+    private String email;
+    @NotBlank(message = "O campo nome da advocacia não pode estar em branco")
+    private String nomeDaAdvocacia;
+
+    public Advocacia(String email, String nomeDaAdvocacia) {
+        this.idAdvocacia = UUID.randomUUID();
+        this.email = email;
+        this.nomeDaAdvocacia = nomeDaAdvocacia;
+    }
+}
