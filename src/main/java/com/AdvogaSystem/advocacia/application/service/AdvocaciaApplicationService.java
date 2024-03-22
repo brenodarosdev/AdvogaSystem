b8f2ee656ecfb4ff20350c4444b2a66c93c66669
Breno Daros <brenosouzadaros@gmail.com>
2024-03-22 17:09:26 -0300

    implementa criacao de credencial de advocacia e dependencias faltantes para autenticacao no pom.xml

--- src/main/java/com/AdvogaSystem/advocacia/application/service/AdvocaciaApplicationService.java
+++ src/main/java/com/AdvogaSystem/advocacia/application/service/AdvocaciaApplicationService.java
@@ -4,6 +4,7 @@ import com.AdvogaSystem.advocacia.application.api.AdvocaciaCriadaResponse;
 import com.AdvogaSystem.advocacia.application.api.AdvocaciaRequest;
 import com.AdvogaSystem.advocacia.application.repository.AdvocaciaRepository;
 import com.AdvogaSystem.advocacia.domain.Advocacia;
+import com.AdvogaSystem.credencial.application.service.CredencialService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AdvocaciaApplicationService implements AdvocaciaService {
     private final AdvocaciaRepository advocaciaRepository;
+    private final CredencialService credencialService;
 
     @Override
     public AdvocaciaCriadaResponse cadastraNovaAdvocacia(AdvocaciaRequest advocaciaNovoRequest) {
         log.info("[inicia] AdvocaciaApplicationService - cadastraNovaAdvocacia");
+        credencialService.criaNovaCredencial(advocaciaNovoRequest);
         Advocacia advocacia = advocaciaRepository.salva(new Advocacia(advocaciaNovoRequest));
         log.info("[finaliza] AdvocaciaApplicationService - cadastraNovaAdvocacia");
         return new AdvocaciaCriadaResponse(advocacia);
